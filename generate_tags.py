9ede6ec49b581ff7edc995e86783d0c11484e9da
Suresh Sonwane <sonwanesuresh95@gmail.com>
2020-09-24 01:30:30 +0530

    Update generate_tags.py

--- generate_tags.py
+++ generate_tags.py
@@ -3,6 +3,8 @@ import nltk
 from nltk import pos_tag
 
 
+# function for generating pos tags
 def generate_tags(text):
+    text = re.sub(r'[^\w\s]+', ' ', text)
     text = pos_tag(text.split())
     return text
